18f5c4ab1feb413ce75d757ffd086fa000a140de
Elijah Insua <tmpvar@gmail.com>
2010-05-08 02:02:30 -0400

    integrated node-router, ready to start serving dual-side templates

--- server/run.js
+++ server/run.js
@@ -5,19 +5,43 @@
   this example will still work.  js on the browser certainly helps performance though!
 */
 
+// Add server/lib to the search path
+require.paths.unshift(__dirname + "/lib");
+require.paths.unshift(__dirname + "/../browser/");
 
-var http = require("http"),
+var port = 10017;
+    sys = require("sys"),
+    dom = require("jsdom/lib/level1/core").dom.level1.core,
+    fs = require("fs"),
+    router = require("node-router/lib/node-router"),
+    server = router.getServer();
+    
+var window = global.window = require("jsdom/lib/browser").windowAugmentation(dom);
+var document = global.document = global.window.document;
+var location = global.location = global.window.location;
+var window = global;
+var navigator = global.navigator = { userAgent: "node-js" };
 
-routes = [
+// TODO: move into level 2
+global.window.document.compareDocumentPosition = function() {};
+dom.Node.prototype.addEventListener = 
+            window.addEventListener = 
+   window.document.addEventListener = function() {};
+
+eval(fs.readFileSync(__dirname + "/../browser/jquery.js"));
+eval(fs.readFileSync(__dirname + "/../browser/pure.js"));
+
+server.get(new RegExp(/\/c\/.+\..+/), router.staticDirHandler(__dirname + "/../browser/", "/c/"));
+
+server.get("/", function(req, res) {
   
-];
-
-function route(req) {
-  
-}
-
-http.createServer(8080, function(err, req, res) {
-
-
+  return "";
 });
 
+// Get a contact 
+server.get(new RegExp(/\/contact\/[\d]+/), function(req, res) {
+  
+  return "";
+});
+
+server.listen(port);
