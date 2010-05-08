bef0e29cecef4723a6073fbc844eed3a9c0c07e7
Elijah Insua <tmpvar@gmail.com>
2010-05-07 20:32:10 -0400

    basic structure, some libs

---
+++ server/run.js
@@ -0,0 +1,23 @@
+/*
+  This is a simple rest service that handles contact list manipulation.
+
+  Templating is unobtrusive which means with or without js on the browser
+  this example will still work.  js on the browser certainly helps performance though!
+*/
+
+
+var http = require("http"),
+
+routes = [
+  
+];
+
+function route(req) {
+  
+}
+
+http.createServer(8080, function(err, req, res) {
+
+
+});
+
