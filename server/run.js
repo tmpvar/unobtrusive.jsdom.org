74753d79bd8da3beed19faebc94ebd8d4fa74754
Elijah Insua <tmpvar@gmail.com>
2010-05-08 23:55:29 -0400

    preloading all of the templates

--- server/run.js
+++ server/run.js
@@ -9,27 +9,49 @@
 require.paths.unshift(__dirname + "/lib");
 require.paths.unshift(__dirname + "/../browser/");
 
-var port = 10017;
-    sys = require("sys"),
-    dom = require("jsdom/lib/level1/core").dom.level1.core,
-    fs = require("fs"),
-    router = require("node-router/lib/node-router"),
-    server = router.getServer();
-    
-var window = global.window = require("jsdom/lib/browser").windowAugmentation(dom);
-var document = global.document = global.window.document;
-var location = global.location = global.window.location;
-var window = global;
-var navigator = global.navigator = { userAgent: "node-js" };
+var port         = 10017,
+    sys          = require("sys"),
+    dom          = require("jsdom/lib/level1/core").dom.level1.core,
+    browser      = require("jsdom/lib/browser"),
+    fs           = require("fs"),
+    router       = require("node-router/lib/node-router"),
+    server       = router.getServer(),
+    jQueryScript = fs.readFileSync(__dirname + "/../browser/jquery.js"),
+    PureScript   = fs.readFileSync(__dirname + "/../browser/pure.js");
 
 // TODO: move into level 2
-global.window.document.compareDocumentPosition = function() {};
-dom.Node.prototype.addEventListener = 
-            window.addEventListener = 
-   window.document.addEventListener = function() {};
+dom.Document.prototype.compareDocumentPosition = function() {};
+dom.Node.prototype.addEventListener = function() {};
 
-eval(fs.readFileSync(__dirname + "/../browser/jquery.js"));
-eval(fs.readFileSync(__dirname + "/../browser/pure.js"));
+var templateToDom = function(path) {
+  var data   = fs.readFileSync(__dirname + "/" + path),
+      window = browser.windowAugmentation(dom);
+  window.addEventListener = function() {};
+  window.document.body.innerHTML = data;
+  window.window = window;
+  with(window) {
+    try {
+      eval(jQueryScript);
+      eval(PureScript);
+    } catch (e) {
+      sys.puts(sys.inspect(e.stack, true));
+    }
+  };
+  
+  return window;
+};
+
+// Cache all of the templates synchronously
+var templates = {
+  master    : templateToDom("template/master.html"),
+  index     : templateToDom("template/index.html"), 
+  error     : templateToDom("template/error.html"),
+  contact   : {
+    list   : templateToDom("template/contact/list.html"),
+    view   : templateToDom("template/contact/view.html"),
+    manage : templateToDom("template/contact/manage.html")
+  }
+}
 
 server.get(new RegExp(/\/c\/.+\..+/), router.staticDirHandler(__dirname + "/../browser/", "/c/"));
 
